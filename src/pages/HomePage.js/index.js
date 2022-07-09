947720671973b4c8d803eb77314f2ccdd4088803
Majedul Hasan <77298488+Majedul-Hasan@users.noreply.github.com>
2022-07-09 22:07:27 +0600

    project initial commit

---
+++ src/pages/HomePage.js/index.js
@@ -0,0 +1,15 @@
+import React from "react";
+import HeaderComponent from "../../components/HeaderComponents";
+
+
+const HomePage = () => {
+  return (
+  <div>  
+  <HeaderComponent />
+  
+  </div>
+  
+  )
+};
+
+export default HomePage;
