286c7857781e871311fb9828f8b1b6d070ebe371
Majedul Hasan <77298488+Majedul-Hasan@users.noreply.github.com>
2022-07-16 02:23:15 +0600

    bottom nav

--- src/svg/index.js
+++ src/svg/index.js
@@ -4,9 +4,13 @@ import Chat from "./chat";
 import ClipBoard from "./ClipBoard";
 import HandbagFill from "./HandbagFill";
 import Heart from "./heart";
+import HomeFill from "./HomeFill";
 import Track from "./Track";
+import User from "./User";
 
 export {
+    User,
+    HomeFill,    
     ClipBoard,
     BoxFill,
     Box,
