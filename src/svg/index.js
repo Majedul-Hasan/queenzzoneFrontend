3846cb1ba149857424b295c23448418b424e278f
Majedul Hasan <77298488+Majedul-Hasan@users.noreply.github.com>
2022-07-16 01:10:33 +0600

    svg icons

---
+++ src/svg/index.js
@@ -0,0 +1,17 @@
+import Box from "./Box";
+import BoxFill from "./BoxFill";
+import Chat from "./chat";
+import ClipBoard from "./ClipBoard";
+import HandbagFill from "./HandbagFill";
+import Heart from "./heart";
+import Track from "./Track";
+
+export {
+    ClipBoard,
+    BoxFill,
+    Box,
+    Track,
+    HandbagFill,
+    Chat,
+    Heart,
+}
